af03fb88f44e7a57679c96b10aaf39ef15a54415
Trong Tuan Vuong <68016861+willvuong168@users.noreply.github.com>
2025-08-02 08:27:23 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
+    allowedHosts: ["4413groupa.me"],
     proxy: {
       "/api": {
         target: "https://localhost:8080",
